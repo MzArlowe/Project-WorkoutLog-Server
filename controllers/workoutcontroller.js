1256d2fc7ac6f5497386da57b34a3d2f598ec939
unknown <bhm727@yahoo.com>
2021-11-09 15:26:09 -0500

    New Update

--- controllers/workoutcontroller.js
+++ controllers/workoutcontroller.js
@@ -4,7 +4,7 @@ let validateJWT = require("../middleware/validate-jwt");
 const { WorkOutModel } = require("../models");
 
 router.get('/workout', (req, res) => { //Injected as a middleware function, tt will check to see if the incoming request has a token for this specific route. Perfect if we have a controller where a specific number of routes needs to be restricted. 
-    res.send('Hey!! This is a practice route!')
+    res.send('Workout Day 2 done!')
 });
 
 module.exports = router;
